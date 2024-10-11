6a3712826a17322749908cc55adec4cfb1219102
sagarhudge <sagar.hudge@gmail.com>
2024-10-11 16:00:49 +0530

    fuse

--- SnapLineChart/src/d3/CircuitDiagramD3.jsx
+++ SnapLineChart/src/d3/CircuitDiagramD3.jsx
@@ -9,9 +9,28 @@ import camera from './camera.svg';
 const circuitData = {
     structures: [
         {
+            id: 0,
+            elements: [
+                // { type: "busbar", orientation: "horizontal", x: 180, y: 100, length: 60, visible: true },
+                // { type: "camera", x: 240, y: 88, length: 25, visible: true },
+                // { type: "busbar", orientation: "horizontal", x: 265, y: 100, length: 90, visible: true },
+                // { type: "openSwitch", x: 280, y: 96, orientation: "right", length: 50, visible: true },
+                // { type: "ground", x: 260, y: 110, orientation: "left", length: 30, visible: true },
+                // { type: "busbar", orientation: "vertical", x: 305, y: 143, length: 50, visible: true },
+                // { type: "text", x: 290, y: 210, content: "1200", visible: true },
+                // { type: "busbar", orientation: "vertical", x: 305, y: 215, length: 200, visible: true },
+                // { type: "busbar", orientation: "horizontal", x: 305, y: 265, length: -70, visible: true },
+                // { type: "busbar", orientation: "vertical", x: 235, y: 334, length: -70, visible: true },
+                // { type: "curve", x: 160, y: 314, length: 150, visible: true },
+                // { type: "halfcurve", x: 290, y: 430, length: 30, visible: true },
+                // // { type: "dotBox",  x: 180, y: 0, width: 200, row1Height: 150, row2Height: 150 },
+                // { type: "dotBox", x: 205, y: 55, row1Height: 130, row2Height: 190, width: 120, visible: true },
+                { type: "redBox", x: 165, y: 35, row1Height: 150, row2Height: 230, width: 180, visible: true },
+            ]
+        }, {
             id: 1,
             elements: [
-                { type: "busbar", orientation: "horizontal", x: 180, y: 100, length: 60, visible: true },
+                { type: "busbar", orientation: "horizontal", x: 170, y: 100, length: 70, visible: true },
                 { type: "camera", x: 240, y: 88, length: 25, visible: true },
                 { type: "busbar", orientation: "horizontal", x: 265, y: 100, length: 90, visible: true },
                 { type: "openSwitch", x: 280, y: 96, orientation: "right", length: 50, visible: true },
@@ -24,10 +43,10 @@ const circuitData = {
                 { type: "curve", x: 160, y: 314, length: 150, visible: true },
                 { type: "halfcurve", x: 290, y: 430, length: 30, visible: true },
                 // { type: "dotBox",  x: 180, y: 0, width: 200, row1Height: 150, row2Height: 150 },
-                { type: "dotBox", x: 180, y: 55, row1Height: 130, row2Height: 190, width: 150, visible: true },
+                { type: "dotBox", x: 205, y: 55, row1Height: 130, row2Height: 190, width: 120, visible: true },
+                { type: "redBox", x: 170, y: 35, row1Height: 150, row2Height: 230, width: 180, visible: true },
             ]
-        },
-        {
+        },  {
             id: 2,
             elements: [
                 { type: "busbar", orientation: "horizontal", x: 180, y: 100, length: 60, visible: true },
@@ -42,8 +61,9 @@ const circuitData = {
                 { type: "busbar", orientation: "vertical", x: 235, y: 334, length: -70, visible: true },
                 { type: "curve", x: 160, y: 314, length: 150, visible: true },
                 { type: "halfcurve", x: 290, y: 430, length: 30, visible: true },
-                { type: "dotBox", x: 180, y: 55, row1Height: 130, row2Height: 190, width: 150, visible: true },
-
+                // { type: "dotBox",  x: 180, y: 0, width: 200, row1Height: 150, row2Height: 150 },
+                { type: "dotBox", x: 205, y: 55, row1Height: 130, row2Height: 190, width: 120, visible: true },
+                { type: "redBox", x: 175, y: 35, row1Height: 150, row2Height: 230, width: 180, visible: true },
             ]
         }
     ]
@@ -78,13 +98,15 @@ const CircuitDiagramD3 = () => {
                 case "curve":
                     drawCurve(element, offsetX);
                     break;
+
                 case "halfcurve":
                     drawHalfCurve(element, offsetX);
-                case "halfcurve":
-                    drawCurve(element, offsetX);
                     break;
                 case "dotBox":
                     drawDottedBox(element, offsetX);
+                    break;
+                case "redBox":
+                    drawRedBox(element, offsetX);
                     break;
                 default:
                     break;
@@ -195,14 +217,14 @@ const CircuitDiagramD3 = () => {
             });
         };
 
-        const drawDottedBox = ({ x, y, row1Height, row2Height, width }, offsetX) => {
+        const drawDottedBox = ({ x, y, row1Height, row2Height, width }, offsetX, color = "green") => {
             // Draw the top edge of the box
             svg.append('line')
                 .attr('x1', x + offsetX)
                 .attr('y1', y)
                 .attr('x2', x + offsetX + width)
                 .attr('y2', y)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -212,7 +234,7 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y)
                 .attr('x2', x + offsetX)
                 .attr('y2', y + row1Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -222,7 +244,7 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y)
                 .attr('x2', x + offsetX + width)
                 .attr('y2', y + row1Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -232,7 +254,7 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y + row1Height)
                 .attr('x2', x + offsetX + width)
                 .attr('y2', y + row1Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -242,7 +264,7 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y + row1Height + row2Height)
                 .attr('x2', x + offsetX + width)
                 .attr('y2', y + row1Height + row2Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -252,7 +274,7 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y + row1Height)
                 .attr('x2', x + offsetX)
                 .attr('y2', y + row1Height + row2Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
 
@@ -262,9 +284,67 @@ const CircuitDiagramD3 = () => {
                 .attr('y1', y + row1Height)
                 .attr('x2', x + offsetX + width)
                 .attr('y2', y + row1Height + row2Height)
-                .attr('stroke', 'green')
+                .attr('stroke', color)
                 .attr('stroke-width', 1.5)
                 .attr('stroke-dasharray', '5,5');
+        };
+
+        const drawRedBox = ({ x, y, row1Height, row2Height, width }, offsetX, color = "red") => {
+            // Draw the top edge of the box
+            svg.append('line')
+                .attr('x1', x + offsetX)
+                .attr('y1', y)
+                .attr('x2', x + offsetX + width)
+                .attr('y2', y)
+                .attr('stroke', 'red')
+                .attr('stroke-width', 1.5)
+
+            // Draw the left side of the top row
+            svg.append('line')
+                .attr('x1', x + offsetX)
+                .attr('y1', y)
+                .attr('x2', x + offsetX)
+                .attr('y2', y + row1Height)
+                .attr('stroke', color)
+                .attr('stroke-width', 1.5)
+
+            // Draw the right side of the top row
+            svg.append('line')
+                .attr('x1', x + offsetX + width)
+                .attr('y1', y)
+                .attr('x2', x + offsetX + width)
+                .attr('y2', y + row1Height)
+                .attr('stroke', color)
+                .attr('stroke-width', 1.5)
+
+             
+
+            // Draw the bottom edge of the box
+            svg.append('line')
+                .attr('x1', x + offsetX)
+                .attr('y1', y + row1Height + row2Height)
+                .attr('x2', x + offsetX + width)
+                .attr('y2', y + row1Height + row2Height)
+                .attr('stroke', color)
+                .attr('stroke-width', 1.5)
+
+            // Draw the left side of the bottom row
+            svg.append('line')
+                .attr('x1', x + offsetX)
+                .attr('y1', y + row1Height)
+                .attr('x2', x + offsetX)
+                .attr('y2', y + row1Height + row2Height)
+                .attr('stroke', color)
+                .attr('stroke-width', 1.5)
+
+            // Draw the right side of the bottom row
+            svg.append('line')
+                .attr('x1', x + offsetX + width)
+                .attr('y1', y + row1Height)
+                .attr('x2', x + offsetX + width)
+                .attr('y2', y + row1Height + row2Height)
+                .attr('stroke', color)
+                .attr('stroke-width', 1.5)
         };
 
 
